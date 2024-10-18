fddc9277d237a518e7c7213c991828c57e4eb82b
dylonk <mechaplatypus25@gmail.com>
2024-10-18 14:00:54 -0500

    fixed login: backend now runs on port 5000

--- frontend/public/js/login.js
+++ frontend/public/js/login.js
@@ -1,15 +1,16 @@
 document.getElementById('login-form').addEventListener('submit', async function(event) {
     event.preventDefault(); // Prevent the default form submission
-
     const form = event.target;//gets the form targheted by the event.
     const formData = new FormData(form);//creates a formdata object from the form.
     const data = {  //creates an object containing the form data.
         username: formData.get('username'),
         password: formData.get('password')
+
+        
     };
 
     try {
-        const response = await fetch('/auth/login', {   //sends a post to auth/login with the form data
+        const response = await fetch('http://localhost:5000/auth/login', {   //sends a post to auth/login with the form data
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
