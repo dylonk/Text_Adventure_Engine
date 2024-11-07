4424ab06c95aa3ff0cffbbd904f83841c1b4797f
Timothy Erbert <timjsherbert@gmail.com>
2024-11-07 02:37:19 -0600

    Set up start paage

---
+++ frontend/src/components/editor/Editor.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+<!---This is the outermost layer of the editor--->
+<template>
+    <div class="editor-container">
+
+    </div>
+</template>
+
+<style>
+.editor-container{
+    width:100vw;
+    height:100vh;
+    display:flex;
+    flex-direction:row;
+    background:red;
+}
+</style>
